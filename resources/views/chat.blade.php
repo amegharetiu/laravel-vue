eccbd3507d1623ebce68d9ebae8d8e920fe0f0f4
Amit Meghare <ameghare@tiuconsulting.com>
2017-02-10 12:31:08 +0530

    This is initial commit

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Chatroom</title>
+
+        <link rel="stylesheet" href="public/css/app.css">
+    </head>
+    <body>
+        <h1>Chatrrom</h1>
+
+        <div id="app">
+            <example></example>
+        </div>
+
+        <script src="public/js/app.js" charset="utf-8"></script>
+    </body>
+</html>
